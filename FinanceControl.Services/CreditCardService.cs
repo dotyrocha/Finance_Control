21e009a76bc366e9389977c98681d367b953f978
André Rocha <79481398+dotyrocha@users.noreply.github.com>
2022-05-09 12:19:21 -0300

    Add files via upload

---
+++ FinanceControl.Services/CreditCardService.cs
@@ -0,0 +1,33 @@
+﻿using FinanceControl.Domain.Models;
+using FinanceControl.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinanceControl.Services
+{
+    public class CreditCardService
+    {
+        private CreditCardRepository creditCardRepository;
+
+        public CreditCardService()
+        {
+            this.creditCardRepository = new CreditCardRepository();
+
+        }
+
+        public void Create(CreditCard entity)
+        {
+
+            // Regras de negócio nessa classe
+
+            creditCardRepository.Insert(entity);
+        }
+
+        public IEnumerable<CreditCard> GetAll()
+        {
+            return creditCardRepository.All().ToList();
+        }
+    }
+}
